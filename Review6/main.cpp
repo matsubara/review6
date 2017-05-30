4c2f6699153a4f926fb24977c6905bba940082d4
Mauricio Matsubara <mauricio@matsubara.co>
2017-05-30 18:34:04 -0300

    exercise desc

--- Review6/main.cpp
+++ Review6/main.cpp
@@ -6,10 +6,21 @@
 //  Copyright © 2017 Mauricio Matsubara. All rights reserved.
 //
 
+/*
+ 
+ 1. Test the initial version of the program
+ 2. Change to retrieve two values and assign them to variables
+    If the first value is higher print a phrase (similar to previous exercise)
+ 3. Change the code to multiply those values and print the result
+ */
+
+
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    int a;
+    std::cin >> a;
+    std::cout << "Your input:" << a;
     return 0;
 }
